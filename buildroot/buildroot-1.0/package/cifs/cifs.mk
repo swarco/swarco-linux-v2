fd4518c6e38945e285f23fc38587bf370861a8ee
Guido Classen <clagix@gmail.com>
2007-02-12 13:23:48 +0000

    bugfix cifs.mk

--- buildroot/buildroot-1.0/package/cifs/cifs.mk
+++ buildroot/buildroot-1.0/package/cifs/cifs.mk
@@ -1,58 +1,46 @@
-#############################################################
+##############################################################################
 #
-# CIFS
+# mount.cifs
 #
-#############################################################
+##############################################################################
+CIFS_VER:=6103
+CIFS_SOURCE:=mount.cifs.c
 
-CIFS_MOUNT_REVISION=6103
+CIFS_URL:='http://viewcvs.samba.org/cgi-bin/viewcvs.cgi/*checkout*/branches/SAMBA_3_0/source/client/mount.cifs.c?rev=17605'
+CIFS_DIR:=$(BUILD_DIR)/cifs
+CIFS_BINARY:=mount.cifs
+CIFS_TARGET_BINARY:=/sbin/$(CIFS_BINARY)
 
-# Don't alter below this line unless you (think) you know
-# what you are doing! Danger, Danger!
+$(DL_DIR)/$(CIFS_SOURCE):
+	$(WGET) -O $(DL_DIR)/$(CIFS_SOURCE)  $(CIFS_URL)
 
-CIFS_SITE=http://websvn.samba.org/cgi-bin/viewcvs.cgi/*checkout*/branches/SAMBA_3_0/source/client
-CIFS_MOUNT_SOURCE=$(CIFS_SITE)/mount.cifs.c?rev=$(CIFS_MOUNT_REVISION)
-CIFS_DIR=$(BUILD_DIR)/cifs
-CIFS_DLDIR=$(DL_DIR)/cifs
-CIFS_TARGET_BINARY:=/sbin/mount.cifs
+$(CIFS_DIR)/$(CIFS_SOURCE): $(DL_DIR)/$(CIFS_SOURCE)
+	test -d $(CIFS_DIR) || mkdir $(CIFS_DIR)
+	cp $(DL_DIR)/$(CIFS_SOURCE) $(CIFS_DIR)
 
-
-$(CIFS_DIR) $(CIFS_DLDIR):
-	mkdir -p $@
-
-$(CIFS_DLDIR)/.downloaded: $(CIFS_DLDIR)
-	$(WGET) -P $(CIFS_DLDIR) -O $(CIFS_DLDIR)/mount.cifs.c $(CIFS_MOUNT_SOURCE)
-	touch $@
-
-$(CIFS_DIR)/.unpacked:	$(CIFS_DLDIR)/.downloaded $(CIFS_DIR)
-	cp -a $(CIFS_DLDIR)/*.c $(CIFS_DIR)/
-	touch $@
-
-
-$(CIFS_DIR)/mount.cifs:	$(CIFS_DIR)/mount.cifs.c
+$(CIFS_DIR)/$(CIFS_BINARY): $(CIFS_DIR)/$(CIFS_SOURCE)
 	$(TARGET_CC) -O2 -o $@ $<
 
-
-$(TARGET_DIR)/$(CIFS_TARGET_BINARY): $(CIFS_DIR)/mount.cifs 
+$(TARGET_DIR)/$(CIFS_TARGET_BINARY):$(CIFS_DIR)/$(CIFS_BINARY)
 	/usr/bin/install -c $(CIFS_DIR)/mount.cifs $(TARGET_DIR)/sbin
-	$(STRIP) $@
+	-$(STRIP) --strip-unneeded $(TARGET_DIR)/$(CIFS_TARGET_BINARY)
 
+cifs: uclibc $(TARGET_DIR)/$(CIFS_TARGET_BINARY)
 
-cifs:	uclibc $(TARGET_DIR)/$(CIFS_TARGET_BINARY)
+cifs_source: $(DL_DIR)/$(CIFS_SOURCE)
 
-cifs-source: $(CIFS_DIR)/.unpacked 
-
-cifs-clean:
+cifs_clean:
 	rm -f $(TARGET_DIR)/$(CIFS_TARGET_BINARY)
 	rm -f $(CIFS_DIR)/mount.cifs $(CIFS_DIR)/mount.cifs.o
 
-cifs-dirclean:
+cifs_dirclean:
 	rm -rf $(CIFS_DIR)
 
-#############################################################
+###############################################################################
 #
 # Toplevel Makefile options
 #
-#############################################################
+###############################################################################
 ifeq ($(strip $(BR2_PACKAGE_CIFS)),y)
 TARGETS+=cifs
 endif
