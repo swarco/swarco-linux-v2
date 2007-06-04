68a16c48d024125c95dc57251cab290e5f252c5b
Guido Classen <clagix@gmail.com>
2007-06-04 13:53:58 +0000

    link /dev/rtc if necessary

--- buildroot/buildroot-1.0/ch_conf/etc/hotplug/mdev.sh
+++ buildroot/buildroot-1.0/ch_conf/etc/hotplug/mdev.sh
@@ -8,5 +8,8 @@ mdev -s
 
 mkdir /dev/misc
 ln -s /dev/ccm2200_gpio /dev/misc/ccm2200_gpio
+test ! -c /dev/rtc -a -c /dev/rtc0 && ln -s /dev/rtc0 /dev/rtc
+
 echo /etc/hotplug/ccm2200_hotplug >/proc/sys/kernel/hotplug
+
 
