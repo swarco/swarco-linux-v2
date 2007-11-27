d12d78ddab33b8a44c8868c3b12789ed9d79f3f6
Guido Classen <clagix@gmail.com>
2007-11-27 15:48:51 +0000

    added comgt (gcom) package to buildroot and select it for rootfs

---
+++ buildroot/buildroot-1.0/package/comgt/comgt.mk
@@ -0,0 +1,58 @@
+# Makefile comgt package Weiss-Embedded Linux
+# (package formerly called "gcom")
+# (C) 2007 Guido Classen, Weiss-Electronic GmbH
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+COMGT_VER:=0.3
+COMGT_SOURCE:=comgt-$(COMGT_VER).tgz
+COMGT_SITE:=http://downloads.openwrt.org/sources/
+COMGT_DIR:=$(BUILD_DIR)/comgt-$(COMGT_VER)
+COMGT_CAT:=zcat
+COMGT_BINARY:=$(COMGT_DIR)/comgt
+COMGT_TARGET_BINARY:=$(TARGET_DIR)/usr/bin/comgt
+COMGT_PDIR=package/comgt
+
+$(DL_DIR)/$(COMGT_SOURCE):
+	 $(WGET) -P $(DL_DIR) $(COMGT_SITE)/$(COMGT_SOURCE)
+
+comgt-source: $(DL_DIR)/$(COMGT_SOURCE)
+
+$(COMGT_DIR)/.unpacked: $(DL_DIR)/$(COMGT_SOURCE)
+	$(COMGT_CAT) $(DL_DIR)/$(COMGT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	patch -p1 -d $(COMGT_DIR) < package/comgt/001-Makefile.patch
+	touch $(COMGT_DIR)/.unpacked
+
+$(COMGT_DIR)/.configured: $(COMGT_DIR)/.unpacked
+	touch $@
+
+$(COMGT_BINARY): $(COMGT_DIR)/.configured
+	$(MAKE) -C $(COMGT_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		gcom
+	touch $@
+
+$(COMGT_TARGET_BINARY): $(COMGT_BINARY)
+	cp -af $(COMGT_DIR)/gcom $(COMGT_TARGET_BINARY)
+	$(STRIP) $(COMGT_TARGET_BINARY)
+
+comgt: uclibc $(COMGT_TARGET_BINARY)
+
+comgt-clean:
+	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(COMGT_DIR) uninstall
+	-$(MAKE) -C $(COMGT_DIR) clean
+
+comgt-dirclean:
+	rm -rf $(COMGT_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_COMGT)),y)
+TARGETS+=comgt
+endif
