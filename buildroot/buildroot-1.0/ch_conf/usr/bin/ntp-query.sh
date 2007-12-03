e6610b00bedb6b244b2f688aae8c678a0ffa9010
Guido Classen <clagix@gmail.com>
2007-12-03 17:53:56 +0000

    cleanup and added comments in ch_conf directory

--- buildroot/buildroot-1.0/ch_conf/usr/bin/ntp-query.sh
+++ buildroot/buildroot-1.0/ch_conf/usr/bin/ntp-query.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
+#*****************************************************************************
+#* 
+#*  @file          ntp-query.sh
+#*
+#*  Run NTP timeserver query as background process. 
+#*  The ntpdate utility can run for several minutes until it has gotten
+#*  a valid time in particular on a slow network connection (GPRS), a
+#*  large timeout (-t option) and if many NTP-hosts are specified.
+#*
+#*  @version       1.0 (\$Revision$)
+#*  @author        Guido Classen <br>
+#*                 Weiss-Electronic GmbH
+#* 
+#*  $LastChangedBy$  
+#*  $Date$
+#*  $URL$
+#*
+#*  @par Modification History:
+#*    2007-12-03 gc: initial version
+#*  
+#*****************************************************************************
 
 NTPDATE=/usr/bin/ntpdate
 HWCLOCK=/sbin/hwclock
@@ -11,6 +32,6 @@ test -f /etc/default/ntpdate || exit 0
 
 test -n "$NTPSERVERS" || exit 0
 
-logger "$0 Running ntpdate to synchronize clock"
+logger -t $0 "Running ntpdate to synchronize clock"
 $NTPDATE $NTPOPTIONS $NTPSERVERS && $HWCLOCK -w
-logger "$0 ntpdate finished with: `date`"
+logger -t $0 "ntpdate finished with: `date`"
--- buildroot/buildroot-1.0/ch_conf/usr/sbin/ssh_keygen.sh
+++ buildroot/buildroot-1.0/ch_conf/usr/sbin/ssh_keygen.sh
@@ -5,19 +5,20 @@
 #*
 #*  /etc/init.d    script initializes the ssh keys on first system boot 
 #*
-#*  @version       1.0 (\$Revision: 1818 $)
+#*  @version       1.0 (\$Revision$)
 #*  @author        Markus Forster <br>
 #*                 Weiss-Electronic GmbH
 #* 
-#*  $LastChangedBy: fom9tr $  
-#*  $Date: 2006-08-27 $
-#*  $URL:  $
+#*  $LastChangedBy$  
+#*  $Date$
+#*  $URL$
 #*
 #*  @par Modification History:
 #*    2006-08-27 mf: initial version (unixdef)
 #*
 #*  
 #*****************************************************************************
+
 SSH_KEYGEN=/usr/bin/ssh-keygen
 HOST_KEY_RSA1=/etc/ssh_host_key
 HOST_KEY_DSA=/etc/ssh_host_dsa_key
@@ -27,22 +28,24 @@ HOST_KEY_RSA1_TMP=/tmp/ssh_host_key
 HOST_KEY_DSA_TMP=/tmp/ssh_host_dsa_key
 HOST_KEY_RSA_TMP=/tmp/ssh_host_rsa_key
 
-mount -o remount,rw /
+LOG="logger -t ssh_keygen.sh"
 
-
-# 2007-02-06 gc: start watchdog trigger daemon!
-# 2007-02-22 mf: no longer needed cause of standard ssh key files
-#/sbin/watchdog -t 1 /dev/ccm2200_watchdog
-
-logger "rsa1, dsa and rsa keys for openSSH will be created..."
+$LOG "Starting OpenSSH key generator"
 ${SSH_KEYGEN} -b 1024 -t rsa1 -f ${HOST_KEY_RSA1_TMP} -N ""
 ${SSH_KEYGEN} -b 1024 -t dsa -f ${HOST_KEY_DSA_TMP} -N ""
 ${SSH_KEYGEN} -b 1024 -t rsa -f ${HOST_KEY_RSA_TMP} -N ""
 # change permissions of ssh key files to non other access
-chown ftp:ftp /home/ftp
 #rm /etc/.firstboot
 
-logger "copying new ssh keys to /etc ..."
+$LOG "Copying newly generated OpenSSH keys to /etc"
+
+remount_ro=
+if mount | grep /dev/root | grep '[,(]ro[,)]' >/dev/null 2>&1
+then
+    mount -oremount,rw /
+    remount_ro="mount -oremount,ro /"
+fi 
+
 cp ${HOST_KEY_RSA1_TMP} ${HOST_KEY_RSA1}
 cp ${HOST_KEY_DSA_TMP} ${HOST_KEY_DSA}
 cp ${HOST_KEY_RSA_TMP} ${HOST_KEY_RSA}
@@ -51,12 +54,16 @@ chmod 600 ${HOST_KEY_RSA1}
 chmod 600 ${HOST_KEY_DSA}
 chmod 600 ${HOST_KEY_RSA}
 
+#???
+chown ftp:ftp /home/ftp
+rm /etc/.firstboot
+
+
+$remount_ro
+
+$LOG "Finished generating OpenSSH keys"
+
 rm ${HOST_KEY_RSA1_TMP}*
 rm ${HOST_KEY_DSA_TMP}*
 rm ${HOST_KEY_RSA_TMP}*
 
-
-rm /etc/.firstboot
-mount -o remount,ro /
-
-logger "done.with openSSH-key creation"
