701ff875c2dcfa8981ccb8c63e62174136fdf947
Guido Classen <clagix@gmail.com>
2012-09-19 14:25:54 +0000

    only generate rsa open-ssh host key!

--- buildroot/swarco_ch_conf/usr/sbin/ssh_keygen.sh
+++ buildroot/swarco_ch_conf/usr/sbin/ssh_keygen.sh
@@ -32,8 +32,9 @@ HOST_KEY_RSA_TMP=/tmp/ssh_host_rsa_key
 LOG="logger -p auth.notice -t $0"
 
 $LOG "Starting OpenSSH key generator"
-${SSH_KEYGEN} -q -b 1024 -t rsa1 -f ${HOST_KEY_RSA1_TMP} -N "" -C ""  < /dev/null > /dev/null 2> /dev/null
-${SSH_KEYGEN} -q -b 1024 -t dsa -f ${HOST_KEY_DSA_TMP} -N "" -C ""    < /dev/null > /dev/null 2> /dev/null
+#2012-09-19 gc: only generate rsa host key!
+#${SSH_KEYGEN} -q -b 1024 -t rsa1 -f ${HOST_KEY_RSA1_TMP} -N "" -C ""  < /dev/null > /dev/null 2> /dev/null
+#${SSH_KEYGEN} -q -b 1024 -t dsa -f ${HOST_KEY_DSA_TMP} -N "" -C ""    < /dev/null > /dev/null 2> /dev/null
 ${SSH_KEYGEN} -q -b 1024 -t rsa -f ${HOST_KEY_RSA_TMP} -N "" -C ""    < /dev/null > /dev/null 2> /dev/null 
 
 $LOG "Copying newly generated OpenSSH keys to /etc"
@@ -45,12 +46,12 @@ then
     remount_ro="mount -oremount,ro /"
 fi 
 
-cp ${HOST_KEY_RSA1_TMP} ${HOST_KEY_RSA1}
-cp ${HOST_KEY_DSA_TMP} ${HOST_KEY_DSA}
+#cp ${HOST_KEY_RSA1_TMP} ${HOST_KEY_RSA1}
+#cp ${HOST_KEY_DSA_TMP} ${HOST_KEY_DSA}
 cp ${HOST_KEY_RSA_TMP} ${HOST_KEY_RSA}
 
-chmod 600 ${HOST_KEY_RSA1}
-chmod 600 ${HOST_KEY_DSA}
+#chmod 600 ${HOST_KEY_RSA1}
+#chmod 600 ${HOST_KEY_DSA}
 chmod 600 ${HOST_KEY_RSA}
 
 #???
