49834848a2cf13fbb5241c5ad88cec71c9b20b93
Guido Classen <clagix@gmail.com>
2012-10-11 12:16:16 +0000

    inittab fix S20bootlog -> S20bootlog.sh

--- buildroot/swarco_ch_conf/etc/ppp/gprs-fail.sh
+++ buildroot/swarco_ch_conf/etc/ppp/gprs-fail.sh
@@ -38,6 +38,23 @@ then
     
     if [ \! -z "$GPRS_DEVICE" ]
     then
+        if [ -f /tmp/gprs-watchdog ] ; then
+            . /tmp/gprs-watchdog
+        fi
+        if [ -z "$GPRS_WATCHDOG_COUNT" -o "$GPRS_WATCHDOG_COUNT" -gt 0 ] ; then
+            # shell_gprs_dial.sh musst run more often the gprs-fail.sh
+            # so if the watchdog is not reset by a new shell_gprs_dial.sh
+            # invokation it seems that this script hangs (may on a blocked
+            # modem device)
+            # so the last resort is a reboot on this place!
+            sleep 10
+            killall watchdog
+            reboot            
+        fi
+
+
+        echo GPRS_WATCHDOG_COUNT=1 >/tmp/gprs-watchdog
+
         /bin/kill `/bin/cat /var/run/ppp0.pid`
         sleep 20
         /bin/kill -9 `/bin/cat /var/run/ppp0.pid`
