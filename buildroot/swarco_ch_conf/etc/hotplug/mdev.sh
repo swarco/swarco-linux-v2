ca7fd1bcd8d481240c67bdd3ac630fb3532515e1
Guido Classen <clagix@gmail.com>
2015-03-17 10:32:40 +0000

    hotplug script: fixed race condition. Set hotplug script before primary initialization of /dev. So devices added immediately of running "mdev -s" are found and added now (Problem on KRI system with external USB SSD drive)

--- buildroot/swarco_ch_conf/etc/hotplug/mdev.sh
+++ buildroot/swarco_ch_conf/etc/hotplug/mdev.sh
@@ -4,13 +4,10 @@ mount -t sysfs none /sys
 mount -t tmpfs -o size=512k tmpfs /dev
 mkdir /dev/pts
 mount -t devpts /dev/pts
+echo /etc/hotplug/ccm2200_hotplug >/proc/sys/kernel/hotplug
 mdev -s
 chmod 0666 /dev/tty
 
 mkdir /dev/misc
 ln -s /dev/ccm2200_gpio /dev/misc/ccm2200_gpio
 test ! -c /dev/rtc -a -c /dev/rtc0 && ln -s /dev/rtc0 /dev/rtc
-
-echo /etc/hotplug/ccm2200_hotplug >/proc/sys/kernel/hotplug
-
-
