18054a6c32b4e17fff528968b3a59e4a74e9b43e
Guido Classen <clagix@gmail.com>
2015-06-08 09:10:15 +0000

    gprs-connection-test.sh: added locking

--- buildroot/swarco_ch_conf/etc/ppp/gprs-connection-test.sh
+++ buildroot/swarco_ch_conf/etc/ppp/gprs-connection-test.sh
@@ -19,8 +19,45 @@
 #*  
 #*****************************************************************************
 
+create_lock() {
+    LOCKDIR="/var/lock/$1.lock"
+    PIDFILE="${LOCKDIR}/PID"
+
+    while ! mkdir "${LOCKDIR}" &>/dev/null
+    do
+    # lock failed, now check if the other PID is alive
+        OTHERPID="$(cat "${PIDFILE}")"
+
+    # if cat wasn't able to read the file anymore, another instance probably is
+    # about to remove the lock -- exit, we're *still* locked
+        if [ $? != 0 ]; then
+            echo "lock failed, PID ${OTHERPID} is active" >&2
+            exit 1
+        fi
+
+        if kill -0 $OTHERPID &>/dev/null; then
+        # lock is valid and OTHERPID is active - exit, we're locked!
+            logger -t "$1" "lock failed, PID ${OTHERPID} is active"
+            exit 1
+        fi
+
+    # lock is stale, remove it and restart
+        logger -t "$1" "removing stale lock of nonexistant PID ${OTHERPID}"
+        rm -rf "${LOCKDIR}"
+    done
+
+
+    trap 'ECODE=$?;
+      logger -t "$1" "Removing lock. Exit: $ECODE"
+      rm -rf "${LOCKDIR}"' 0 1 2 3 15
+    echo "$$" >"${PIDFILE}"
+
+}
+
+
 if [ -f /etc/default/gprs ]
 then
+    create_lock gprs-connection-test
 
     . /etc/default/gprs
     
