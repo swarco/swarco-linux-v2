92208f1abf03c07ba4cfbf9195ad2bb82491872d
Guido Classen <clagix@gmail.com>
2017-09-19 22:37:53 +0200

    Added new file headers with GPL License

--- board/swarco/ccm2200/rootfs_overlay/etc/init_common.sh
+++ board/swarco/ccm2200/rootfs_overlay/etc/init_common.sh
@@ -1,19 +1,29 @@
-#*****************************************************************************
-#* 
-#*  @file          /etc/init_common
-#*
-#*                 Common code sourced by init scripts
-#*                 Buildroot uclibc linux
-#*                 2005 Weiss-Electronic GmbH
-#*
-#*  @version       0.1
-#*  @date          <2005-09-05 17:03:03 gc>
-#*  @author        Guido Classen
-#* 
-#*  @par change history:
-#*    2005-09-05 gc: initial version 
-#*  
-#*****************************************************************************
+#
+# /etc/init_common
+#
+# Common code sourced by init scripts (Helper code for module loading)
+#
+#
+# Copyright (C) 2005 by Weiss-Electronic GmbH
+#                       SWARCO Traffic Systems GmbH
+#                       Guido Classen <clagix@gmail.com>
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version
+# 2 or later as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Modification History:
+#    2005-09-05 gc: initial version
+#
 
 
 ##############################################################################
@@ -22,7 +32,7 @@
 load_module() {
     module=$1
     shift
-    if ! lsmod | grep ${module##*/} ; then 
+    if ! lsmod | grep ${module##*/} ; then
       echo loading module: "$module $@"
       $INSMOD $MODULE_PATH/$module$MODULE_SUFF $@
     fi
@@ -33,21 +43,21 @@ load_module() {
 ##############################################################################
 
 
-kernel_version=`uname -r` 
+kernel_version=`uname -r`
 kernel_minor=${kernel_version##*.}
 
 case "$kernel_version" in
-	3.*)
-	        echo detected Kernel Version 3.x
+        3.*)
+                echo detected Kernel Version 3.x
                 INSMOD=/sbin/insmod
                 MODULE_PATH=/lib/modules/$kernel_version
                 MODULE_SUFF=.ko
                 kernel_version_group=2.6
-	;;
+        ;;
 
-	2.6.*)
-	        echo detected Kernel Version 2.6
-		#exec /etc/rcS-2.6
+        2.6.*)
+                echo detected Kernel Version 2.6
+                #exec /etc/rcS-2.6
 #                INSMOD=/usr/bin/insmod.module-init-tools
                 INSMOD=/sbin/insmod
 #                MODULE_PATH=/mnt/weiss/lib/modules/$kernel_version
@@ -60,10 +70,10 @@ case "$kernel_version" in
                 # mount /dev/pts explicitly
                 # mount -t devpts none /dev/pts
 
-	;;
+        ;;
 
-	2.4.*)
-	        echo detected Kernel Version 2.4
+        2.4.*)
+                echo detected Kernel Version 2.4
                 INSMOD=/sbin/insmod
                 MODULE_PATH=/lib/modules/$kernel_version
                 MODULE_SUFF=.o
@@ -74,5 +84,5 @@ case "$kernel_version" in
                     kernel_version_group=2.4-late
                 fi
 
-	;;
+        ;;
 esac
